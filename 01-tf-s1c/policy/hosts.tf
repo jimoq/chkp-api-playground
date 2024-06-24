0362795d96846f21e20581608cb2a2a55c6b9321
Jim Öqvist <44175368+jimoq@users.noreply.github.com>
2024-06-24 13:30:54 +0000

    added ignore_warnings to host reasource

--- 01-tf-s1c/policy/hosts.tf
+++ 01-tf-s1c/policy/hosts.tf
@@ -2,6 +2,7 @@ resource "checkpoint_management_host" "azurelbhealthcheck" {
       ipv4_address = "168.63.129.16"
       name = "${random_id.id.b64_url}-azure_lb_health_check"
       color = "light green"
+      ignore_warnings = "true"
       nat_settings = {
         auto_rule = false
         }
@@ -11,6 +12,7 @@ resource "checkpoint_management_host" "internallb" {
       ipv4_address = "10.99.1.11"
       name = "${random_id.id.b64_url}-internallb"
       color = "blue"
+      ignore_warnings = "true"
       nat_settings = {
         auto_rule = false
         }
@@ -20,9 +22,9 @@ resource "checkpoint_management_host" "rdphost" {
       ipv4_address = "10.99.11.4"
       name = "${random_id.id.b64_url}-rdphost"
       color = "red"
+      ignore_warnings = "true"
       nat_settings = {
         auto_rule = false
         }
-
       }
 
